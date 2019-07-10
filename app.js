cc5529864e2206aa399aad4569ee70f5028c693b
Olivia Thetgyi <50234696+othetgyi@users.noreply.github.com>
2019-07-10 13:46:49 +0100

    Wrote function to display certain text when button is clicked

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-document.getElementById("tasks").innerHTML = "Hello World";
+document.getElementById("enter").addEventListener("click", clicked);
+
+function clicked() {
+    document.getElementById("display").innerHTML = "Hello World!";
+}
+
