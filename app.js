1ffc919473e49771e62a32076a184606f50bb83f
Olivia Thetgyi <50234696+othetgyi@users.noreply.github.com>
2019-07-26 16:48:12 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var button = document.getElementById("enter");
 var item = document.getElementsByTagName("li");
 
+
+//Add a task
 function newTask() {
     var li = document.createElement("li");
     li.appendChild(document.createTextNode(input.value));
@@ -10,7 +12,14 @@ function newTask() {
     } else {
         document.getElementById("tasks").appendChild(li);
     }
+    
     document.getElementById("input").value = "";
+}
+
+//video 41:43
+//Delete a task
+function deleteTask(e){
+   e.target.parentElement.removeChild(e.target);
 }
 
 button.addEventListener("click", newTask);
