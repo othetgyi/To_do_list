c478e9292da58eaff2505a975ccf3bdee59139b8
Olivia Thetgyi <50234696+othetgyi@users.noreply.github.com>
2019-07-26 14:00:03 +0100

    Adding error message if form is blank

--- app.js
+++ app.js
@@ -1,27 +1,29 @@
-var add = document.getElementById("add");
-var input = document.getElementById("userInput");
+var button = document.getElementById("enter");
 var ul = document.querySelector("ul");
+var input = document.getElementById("input");
 var item = document.getElementsByTagName("li");
 
-function inputLength(){
-    return input.value.length;
+function validate() {
+    if (input == "") {
+        alert("Please add a task");
+        return false;
+    }
 }
-//create li and add text to li
-function addTask () {
-    var li = document.createElement("li"); 
+
+
+
+function newTask() {
+    var li = document.createElement("li");
     li.appendChild(document.createTextNode(input.value));
     ul.appendChild(li);
     input.value = "";
 }
 
-//add task after clicking
-function addLi() {
-    if (inputLength() > 0) {
-        addTask();
-    }
-} 
 
-add.addEventListener("click",addLi);
+validate();
+
+button.addEventListener("click", newTask);
 
 
+//addeventlistener when you click on button, runs a function
 
--- todoList.js
+++ todoList.js
@@ -11,3 +11,30 @@ function addTodo(){
 }
 
 button.addEventListener("click", addTodo);
+
+
+//extra code
+
+var add = document.getElementById("add");
+var input = document.getElementById("userInput");
+var ul = document.querySelector("ul");
+var item = document.getElementsByTagName("li");
+
+function inputLength(){
+    return input.value.length;
+}
+//create li and add text to li
+function addTask () {
+    var li = document.createElement("li"); 
+    li.appendChild(document.createTextNode(input.value));
+    ul.appendChild(li);
+}
+
+//add task after clicking
+function addLi() {
+    if (inputLength() > 0) {
+        addTask();
+    }
+} 
+
+add.addEventListener("click",addLi);
