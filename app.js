f13cb98ecf702da607092badc622ad96ce0a1686
Olivia Thetgyi <50234696+othetgyi@users.noreply.github.com>
2019-08-01 16:22:03 +0100

    Deleted li

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 //Select elements
 const clear = document.querySelector(".clear");
 const dateElement = document.getElementById("date");
@@ -19,66 +18,20 @@ function addTodo(toDo){
     const item = `
         <i class="far fa-circle" job="complete" id="0"></i>
         <p class="text">${toDo}</p>
-        <i class="fas fa-trash"></i>
-    
-    `
+        <i class="fas fa-trash" job="delete" id="0"></i>
+        
+    `;
+    const position = "beforeend";
+
+    list.insertAdjacentHTML(position, item);
 }
 
-=======
-// Creating the Close buttons for lists that are not created with the main function
-var myList = document.getElementsByTagName("li");
-var index;
-for(index = 0; index < myList.length; index++) {
-    var aSpanTag = document.createElement("SPAN");
-    var someTxt = document.createTextNode("\u00D7");
-    aSpanTag.className ="close";
-    aSpanTag.appenddChild(someTxt);
-    myList[index].appendChild(aSpanTag);
-}
-var button = document.getElementById("enter");
-var item = document.getElementsByTagName("li");
-// Close Button
-var closeButton = document.getElementsByClassName("close");
-
-
-//Show today's date
-const options = {weekday : "long", month:"short", day:"numeric"};
-const today = new Date();
-
-dateElement.innerHTML = today.toLocaleDateString("en-US", options);
-
-//Create a task
-function newTask() {
-    var li = document.createElement("li");
-    li.appendChild(document.createTextNode(input.value));
-    
-    if(input.value === ""){
-        alert("Please add a task");
-    } else {
-        document.getElementById("tasks").appendChild(li);
+//add a todo when the user hits the enter key
+document.addEventListener("keyup", function(event){
+    if (event.keyCode == 13){
+        
     }
-    document.getElementById("input").value = "";
-    li.onclick = removeItem;
-
-//Create span tag
-var thePanTag = document.createElement("SPAN");
-var txt = document.createTextNode("\u00D7");
-thePanTag.className = "close";
-thePanTag.appendChild(txt);
-li.appendChild(thePanTag);
-
-}
-
-
-
-//Delete todos when you click on li
-function removeItem(e) {
-    event.target.parentElement.removeChild(event.target);
-}
-
-
->>>>>>> 1615a96e773cb5616db5a71dcf702732e4e99661
-
+})
 
 
 
