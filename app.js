8e1e297221628b38af927f782a12cfcaf13c9d6d
Olivia Thetgyi <50234696+othetgyi@users.noreply.github.com>
2019-08-01 16:01:57 +0100

    Added trash can icon

--- app.js
+++ app.js
@@ -6,13 +6,22 @@ const input = document.getElementById("input");
 
 //Class names
 const complete = "far fa-check-circle";
-const unComplete = "far fa-circle";
+const unComplete = "";
 const line_through = "lineThrough";
 
 const options = {weekday : "long", month:"short", day:"numeric"};
 const today = new Date();
 
 dateElement.innerHTML = today.toLocaleDateString("en-UK", options);
+
+function addTodo(toDo){
+    const item = `
+        <i class="far fa-circle" job="complete" id="0"></i>
+        <p class="text">${toDo}</p>
+        <i class="fas fa-trash"></i>
+    
+    `
+}
 
 
 
