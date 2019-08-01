b7f47703031f257c95534a28061df93d1a9d8829
Olivia Thetgyi <50234696+othetgyi@users.noreply.github.com>
2019-08-01 15:30:27 +0100

    Deleted number of functions

--- app.js
+++ app.js
@@ -1,17 +1,7 @@
 // Creating the Close buttons for lists that are not created with the main function
 var myList = document.getElementsByTagName("li");
 var index;
-for(index = 0; index < myList.length; index++) {
-    var aSpanTag = document.createElement("SPAN");
-    var someTxt = document.createTextNode("\u00D7");
-    aSpanTag.className ="close";
-    aSpanTag.appenddChild(someTxt);
-    myList[index].appendChild(aSpanTag);
-}
-var button = document.getElementById("enter");
-var item = document.getElementsByTagName("li");
-// Close Button
-var closeButton = document.getElementsByClassName("close");
+
 
 
 
@@ -26,7 +16,7 @@ document.getElementById("date").innerHTML = today;
 function newTask(toDo) {
     const item = `<li class="item">
         <i class="fa fa-circle-thin co" job="complete" id="0"></i>
-        <p class="text">${toDo}</p>
+        <p class="text">Drink coffee</p>
         <i class="fa fa-trash-o de" job="delete" id="0"></i>
         </li>
         `;
@@ -36,39 +26,11 @@ function newTask(toDo) {
         list.insertAdjacentHTML(position, item);
     }
 
-    var li = document.createElement("li");
-    li.appendChild(document.createTextNode(input.value));
-    
-    if(input.value === ""){
-        alert("Please add a task");
-    } else {
-        document.getElementById("tasks").appendChild(li);
-    }
-    document.getElementById("input").value = "";
-    li.onclick = removeItem;
 
 
-//Create span tag
-var thePanTag = document.createElement("SPAN");
-var txt = document.createTextNode("\u00D7");
-thePanTag.className = "close";
-thePanTag.appendChild(txt);
-li.appendChild(thePanTag);
-}
 
-//Delete todos when you click on li
-function removeItem(e) {
-    event.target.parentElement.removeChild(event.target);
-}
 
-var ulList = document.querySelector("ul");
-ulList.addEventListener("click", function(){
-    if(event.target.tagName === "LI"){
-        event.target.classList.toggle("checked");
-    }
-})
 
-button.addEventListener("click", newTask);
 
 
 
