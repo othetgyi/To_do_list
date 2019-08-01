d22ac5e4876a29827d3323de759d9e808a98bc64
Olivia Thetgyi <50234696+othetgyi@users.noreply.github.com>
2019-08-01 16:46:53 +0100

    Adding FA icon

--- app.js
+++ app.js
@@ -15,23 +15,36 @@ const today = new Date();
 dateElement.innerHTML = today.toLocaleDateString("en-UK", options);
 
 function addTodo(toDo){
-    const item = `
-        <i class="far fa-circle" job="complete" id="0"></i>
-        <p class="text">${toDo}</p>
-        <i class="fas fa-trash" job="delete" id="0"></i>
-        
-    `;
-    const position = "beforeend";
+    var li = document.createTextNode("li");
+    li.appendChild(document.createTextNode(`<i class="far fa-circle" job="complete" id="0"></i>` +  input.value));
 
-    list.insertAdjacentHTML(position, item);
+    if (input.value == ""){
+        alert("Please add a task");
+    }   else {
+        document.getElementById("list").appendChild(li);
+    }
+    input.value = "";
+    
+    //const item = `
+    //    <i class="far fa-circle" job="complete" id="0"></i>${toDo}
+    //    <i class="fas fa-trash" job="delete" id="0"></i>
+        
+    //`;
+   // const position = "beforeend";
+
+    //list.insertAdjacentHTML(position, item);
 }
 
 //add a todo when the user hits the enter key
 document.addEventListener("keyup", function(event){
     if (event.keyCode == 13){
-        
+        const toDo = input.nodeValue;
+        //if the input isn't empty
+        if(toDo){
+            addTodo(toDo);
+        }
     }
-})
+});
 
 
 
